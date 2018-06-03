105120f8aae91e3f24573cb4deaa3a3c90946fb0
Itsme Travel addict Prasanna <39876088+PrasannaKasu@users.noreply.github.com>
2018-06-03 22:07:08 +0530

    First commit

---
+++ app/src/main/java/com/example/android/a8ballgame/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android.a8ballgame;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button askButton;
+        askButton = (Button) findViewById(R.id.ask_button);
+
+        final ImageView eightBall_image;
+        eightBall_image = (ImageView) findViewById(R.id.image_eightBall);
+
+        final int[] ballsArray = {R.drawable.ball1,
+                            R.drawable.ball2,
+                            R.drawable.ball3,
+                            R.drawable.ball4,
+                            R.drawable.ball5};
+        askButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomNum = new Random();
+                int num = randomNum.nextInt(5);
+                eightBall_image.setImageResource(ballsArray[num]);
+
+            }
+        });
+
+
+    }
+}
